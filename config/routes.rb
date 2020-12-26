60e32ee9effc697953e1fe80c77a9fd96c17350a
Le Thanh Viet <vietleuit@gmail.com>
2020-12-26 11:02:11 +0700

    config app: update routes for the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
+  # Simple forum discussion
+  mount SimpleDiscussion::Engine => "/forum"
 
   devise_for :users
   root to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
