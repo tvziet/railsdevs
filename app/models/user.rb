9078d41181b08603601be24a528c0b33ce6cba55
Le Thanh Viet <vietleuit@gmail.com>
2021-01-01 11:13:10 +0700

    updates user model with slug

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  slug                   :string
 #  unconfirmed_email      :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
@@ -26,10 +27,20 @@
 #  index_users_on_confirmation_token    (confirmation_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_slug                  (slug) UNIQUE
 #
 class User < ApplicationRecord
+  # Add simple forum discussio
   include SimpleDiscussion::ForumUser
+
+  # Friendly
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+  # Name of user
   has_person_name
+
+  # Association
   has_many :jobs, dependent: :destroy
 
   # Include default devise modules. Others available are:
@@ -40,5 +51,13 @@ class User < ApplicationRecord
 
   def name
     "#{first_name} #{last_name}"
+  end
+
+  def should_generate_new_friendly_id?
+    if !slug?
+      first_name_changed? || last_name_changed?
+    else
+      false
+    end
   end
 end
