d6a7f4f3ab42d952d2f4591323bc0baa19e39a6e
Le Thanh Viet <vietleuit@gmail.com>
2021-01-01 11:12:44 +0700

    updates schema for user and job models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_26_043544) do
+ActiveRecord::Schema.define(version: 2021_01_01_025154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,13 +99,23 @@ ActiveRecord::Schema.define(version: 2020_12_26_043544) do
     t.string "link_to_apply"
     t.string "company_name"
     t.string "company_website"
-    t.string "role_type"
+    t.string "headquarters"
     t.string "compensation_range"
-    t.boolean "remote", default: false
+    t.string "estimated_hours"
+    t.string "upsell_type"
     t.string "years_of_experience"
+    t.integer "status", default: 0
+    t.integer "compensation_type", default: 0
+    t.integer "price"
+    t.datetime "published_at"
+    t.datetime "featured_until"
+    t.boolean "featured", default: false
+    t.boolean "remote", default: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_jobs_on_slug", unique: true
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
@@ -127,9 +137,11 @@ ActiveRecord::Schema.define(version: 2020_12_26_043544) do
     t.boolean "moderator"
     t.boolean "developer", default: false
     t.boolean "employer", default: false
+    t.string "slug"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
