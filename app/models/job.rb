12bae5fd71eb84b648ec448048d230e32ae18706
Le Thanh Viet <vietleuit@gmail.com>
2021-01-01 11:11:25 +0700

    updates job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,10 +6,19 @@
 #  company_name        :string
 #  company_website     :string
 #  compensation_range  :string
+#  compensation_type   :integer          default("contract")
+#  estimated_hours     :string
+#  featured            :boolean          default(FALSE)
+#  featured_until      :datetime
+#  headquarters        :string
 #  link_to_apply       :string
+#  price               :integer
+#  published_at        :datetime
 #  remote              :boolean          default(FALSE)
-#  role_type           :string
+#  slug                :string
+#  status              :integer          default("pending")
 #  title               :string
+#  upsell_type         :string
 #  years_of_experience :string
 #  created_at          :datetime         not null
 #  updated_at          :datetime         not null
@@ -17,6 +26,7 @@
 #
 # Indexes
 #
+#  index_jobs_on_slug     (slug) UNIQUE
 #  index_jobs_on_user_id  (user_id)
 #
 # Foreign Keys
@@ -24,8 +34,31 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Job < ApplicationRecord
-  belongs_to :user
+  # Friendly
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  # Richtext
   has_rich_text :description
   has_rich_text :company_description
+
+  # ActiveStorage
   has_one_attached :company_logo
+
+  # Association
+  belongs_to :user
+
+  # Define status of job using enum
+  enum status: { pending: 0, published: 1, archived: 2 }
+
+  # Define compensation type of job using enum
+  enum compensation_type: { contract: 0, fulltime: 1 }
+
+  def should_generate_new_friendly_id?
+    if !slug?
+      title_changed?
+    else
+      false
+    end
+  end
 end
--- db/migrate/20201226041414_create_jobs.rb
+++ db/migrate/20201226041414_create_jobs.rb
@@ -5,10 +5,18 @@ class CreateJobs < ActiveRecord::Migration[6.0]
       t.string :link_to_apply
       t.string :company_name
       t.string :company_website
-      t.string :role_type
+      t.string :headquarters
       t.string :compensation_range
-      t.boolean :remote, default: false
+      t.string :estimated_hours
+      t.string :upsell_type
       t.string :years_of_experience
+      t.integer :status, default: 0
+      t.integer :compensation_type, default: 0
+      t.integer :price
+      t.datetime :published_at
+      t.datetime :featured_until
+      t.boolean :featured, default: false
+      t.boolean :remote, default: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
